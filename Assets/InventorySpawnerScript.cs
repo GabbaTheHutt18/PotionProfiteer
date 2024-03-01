2fc3efe9fe2818ae2161530afe550c15a02f6001
GabbaTheHutt18 <91136983+GabbaTheHutt18@users.noreply.github.com>
2024-03-01 16:44:58 +0000

    01/03/2024
    A (hopefully) successful merge

--- Assets/InventorySpawnerScript.cs
+++ Assets/InventorySpawnerScript.cs
@@ -24,7 +24,6 @@ public class InventorySpawnerScript : MonoBehaviour
         LogicManager = GameObject.FindGameObjectWithTag("Logic").GetComponent<LogicManagerScript>();
         foreach (var item in LogicManager.Potions)
         {
-            Debug.Log("hi");
             SpawnPotion(item);
         }
     }
