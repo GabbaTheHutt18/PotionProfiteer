e2997228fab66077b4273314523ddceeb88185c5
UnknownGamer925 <joemcd.business@gmail.com>
2024-04-30 03:18:03 +0100

    29/04/24 - Grassland Tileset
    Added grassland tileset & grassland art
    
    modified spawn code to account for invalid tiles

--- Assets/Resources/Spawnables/SpawnResources.cs
+++ Assets/Resources/Spawnables/SpawnResources.cs
@@ -2,9 +2,11 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Runtime.CompilerServices;
 using Unity.Burst.Intrinsics;
+using Unity.VisualScripting;
 using Unity.VisualScripting.ReorderableList.Element_Adder_Menu;
 using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine.Tilemaps;
 
 public class SpawnResources : MonoBehaviour
 {
@@ -19,6 +21,8 @@ public class SpawnResources : MonoBehaviour
     [SerializeField] int y_min; // Defines spawnbox - bottom edge
     [SerializeField] int y_max; // Defines spawnbox - top edge
 
+    [SerializeField] Tilemap Tmap;
+
     void Start()
     {
         GameObject[] rarities = { common, rare, epic, legendary };
@@ -29,9 +33,24 @@ public class SpawnResources : MonoBehaviour
         {
             for (int i = 0; i < loop; i++) // loops int 'loop' number of times, in which the selected object is spawned at a random location within the defined spawnbox.
             {
-                int x = Random.Range(x_min, x_max); // random location
-                int y = Random.Range(y_min, y_max);
-
+                int x = 0;
+                int y = 0;
+                bool valid = false;
+                while (valid == false)
+                {
+                    x = Random.Range(x_min, x_max); // random location
+                    y = Random.Range(y_min, y_max);
+                    if (Tmap.HasTile(Tmap.WorldToCell(new Vector3(x, y, 0))))
+                    {
+                        Debug.Log("InValid tile - rerolling");
+                    }
+                    else
+                    {
+                        Debug.Log("Valid Tile");
+                        valid = true;
+                    }
+                    
+                }
                 GameObject element = Instantiate(rarities[index], this.gameObject.transform); // spawns object
                 element.transform.position = new Vector3(x, y, -1);
             }
